7be03b38d949ce7f36363f97c9f1b1aa002429d1
Michael Calkins <m.calkins576@gmail.com>
2019-10-21 14:26:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -104,7 +104,7 @@ def movies_with_directors_set(source)
         movie_hash[:title] = source[name_index][:movies][movie_index][:title]
       movie_index += 1
     end
-    total_array << movie_array
+    total_array << movie_hash
     name_index += 1
   end
   p total_array
