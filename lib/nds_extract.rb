d132bb3840810a0ce21c869b6c0725c21da3f2f9
Michael Calkins <m.calkins576@gmail.com>
2019-10-21 11:26:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def movies_with_director_key(name, movies_collection)
   new_array = []
   movie_index = 0
   while movie_index < movies_collection.length do
-    new_array[movie_index] << movie_with_director_name(director_name, movie_data)
+    new_array[movie_index] << movie_with_director_name(name, movies_collection)
     movie_index += 1
   end
   pp new_array  
