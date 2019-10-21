3b3079da4b99a9e670743d238e05cd568605783d
Michael Calkins <m.calkins576@gmail.com>
2019-10-21 13:37:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,13 +75,11 @@ def gross_per_studio(collection)
   new_hash = {}
   while movie_index < collection.length do
     studio_gross = 0
+    
     studio_gross += collection[movie_index][:worldwide_gross]
     new_hash[collection[movie_index][:studio]] = studio_gross
     movie_index += 1
-    if collection[movie_index][:studio] ==  collection[movie_index - 1][:studio]
-      studio_gross += collection[movie_index][:worldwide_gross]
-    else
-      new_hash[collection[movie_index][:studio]] = collection[movie_index][:worldwide_gross]
+    
     end
   end
   p new_hash
