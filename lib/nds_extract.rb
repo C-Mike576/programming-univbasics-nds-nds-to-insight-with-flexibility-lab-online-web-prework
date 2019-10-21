bde379a13a96b026c8f2390767a6be7531682fe7
Michael Calkins <m.calkins576@gmail.com>
2019-10-21 14:26:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -94,7 +94,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  p source[0]
+  # p source[0]
   total_array = []
   name_index = 0
   while name_index < source.length do
