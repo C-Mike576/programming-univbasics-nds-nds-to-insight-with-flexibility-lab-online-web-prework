99264e376f6f7c0cff5ab9f6e107c97f663e6690
Michael Calkins <m.calkins576@gmail.com>
2019-10-21 12:20:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,8 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   #p movies_collection[0]
-=begin  
+  puts movies_collection
+
   new_array = []
   new_hash = movies_collection
   movie_index = 0
@@ -59,7 +60,8 @@ def movies_with_director_key(name, movies_collection)
     movie_index += 1
   end
   p new_hash
-=end  
+
+  
 end
 
 
