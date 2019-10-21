b9e4ba771164823d2640d6db88cc80860b9dd03f
Michael Calkins <m.calkins576@gmail.com>
2019-10-21 12:02:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -73,10 +73,19 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-
-
-
-
+  p collection
+=begin
+  name_index = 0
+  while name_index < collection.length do
+    movie_gross = 0
+    movie_index = 0
+    while movie_index < collection[name_index][:movies].length do
+        movie_gross += nds[name_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+        name_index += 1
+  end
+=end  
 end
 
 def movies_with_directors_set(source)
